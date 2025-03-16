96dabe42d921b5fa85fdb1027830da8b94c9bc0e
Anish Gupta <anishhuwu@gmail.com>
2025-03-16 18:15:56 +0530

    02: added shadcn and made layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +24,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="en" className="dark" suppressHydrationWarning>
+      <body className={cn(geistMono.className, "antialiased mx-auto max-w-screen-xl px-4 border min-h-screen")}>
+        <main>{children}</main>
       </body>
     </html>
   );
